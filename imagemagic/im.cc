e55c55bd313be15e68901127419d263a316076a9
Peeter Joot <peeterjoot@pm.me>
2021-01-31 21:52:16 -0500

    im.cc         replace 2x3 image with 40x60 sines and cosines of a colormap.

--- imagemagic/im.cc
+++ imagemagic/im.cc
@@ -1,27 +1,53 @@
 // https://stackoverflow.com/a/36433202/189270
 #include <Magick++.h>
-#include <iostream> 
+#include <iostream>
+#include <cmath>
+#include <algorithm>
 
-using namespace std; 
-using namespace Magick; 
+using namespace std;
+using namespace Magick;
 
-int main(int argc,char **argv) 
-{ 
-   unsigned char pix[]={
-        200,200,200,    100,100,100,
-        0,0,0,          255,0,0,
-        0,255,0,        0,0,255};
+struct RGB
+{
+    uint8_t c[3];
+};
 
-   // Initialise ImageMagick library
-   //InitializeMagick(*argv);
-   InitializeMagick(""); // https://legacy.imagemagick.org/discourse-server/viewtopic.php?t=9581
+// https://stackoverflow.com/a/20792531/189270
+inline RGB mixer( double minimum, double maximum, double value ) {
+  auto ratio = 2 * (value - minimum) / (maximum - minimum);
+  uint8_t b = (uint8_t) std::max(0.0, 255 * (1 - ratio));
+  uint8_t r = (uint8_t) std::max(0.0, 255 * (ratio - 1));
+  uint8_t g = 255 - b - r;
 
-   // Create Image object and read in from pixel data above
-   Image image; 
-   image.read(2,3,"RGB",CharPixel,pix);
+  RGB rc{r, g, b};
 
-   // Write the image to a file - change extension if you want a GIF or JPEG
-   image.write("result.png"); 
+  return rc;
+}
 
-   return 0;
+int main(int argc, char **argv) {
+  int width = 40;
+  int height = 60;
+  RGB pix[width * height];
+
+  for (int j = 0; j < height; j++) {
+    double c = cos( 2 * M_PI * j/height );
+
+    for (int i = 0; i < width; i++) {
+        double s = sin( 2 * M_PI * i/ width );
+
+        pix[j * width + i] = mixer( -1.0, 1.0, s * c );
+    }
+  }
+
+  // Initialise ImageMagick library
+  InitializeMagick( ""); // https://legacy.imagemagick.org/discourse-server/viewtopic.php?t=9581
+
+  // Create Image object and read in from pixel data above
+  Image image;
+  image.read(width, height, "RGB", CharPixel, (unsigned char *)pix);
+
+  // Write the image to a file - change extension if you want a GIF or JPEG
+  image.write("result.png");
+
+  return 0;
 }
